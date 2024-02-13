81bb175e35d9ac64e9ad94b8e0c589202d5dd9e4
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2024-02-14 10:23:11 +1300

    Update digicode1.ino

--- digicode1/digicode1.ino
+++ digicode1/digicode1.ino
@@ -1,3 +1,5 @@
+//Shared with Mr Hansen successfully
+//use the autoformat option in the tools menu
 const byte LEDPIN = 12;
   const byte PAWPIN1 = 1;
   const byte PAWPIN2 = 2;
@@ -9,13 +11,14 @@ const byte LEDPIN = 12;
 Servo leftEar;
 servo rightEar;
 
-  void setup() € pinMode(LEDPIN, OUTPUT);
-  pinMode(PAWPIN1, INPUT)
-    : pinMode(PAWPIN2, INPUT);
-  pinMode(PAWPIN3, INPUT)5
-    : pinMode(PAWPIN4, INPUT);
-    leftEar.attach(lefttappin)
-    rightEar(righttappin)
+  void setup() {
+pinMode(LEDPIN, OUTPUT);
+  pinMode(PAWPIN1, INPUT);
+    pinMode(PAWPIN2, INPUT);
+  pinMode(PAWPIN3, INPUT);
+    pinMode(PAWPIN4, INPUT);
+    leftEar.attach(LEFTEAR5PIN);
+    rightEar.attach(RIGHTEARPIN);
 }
 void loop() {
 
@@ -24,15 +27,15 @@ void loop() {
     digitalWrite(LEDPIN, HIGH);
     ｝
       // If paw 2 is pressed then do the following
-      if （digitalRead （PAWPIN2）＝ = HIGH）｛
+      if（digitalRead(PAWPIN2）＝= HIGH）｛
         digitalWrite(LEDPIN, LOW);
     ｝
       // If paw 3 is pressed then do the following
       if (digitalRead(PAWPIN3) == HIGH) {
       playMusic();
       ｝
-        //
-        If paw 4 is pressed then do the following if (digitalRead(PAWPIN4) == HIGH) {
+        //If paw 4 is pressed then do the following 
+        if (digitalRead(PAWPIN4) == HIGH) {
         int score = playGame();
       }
     }
